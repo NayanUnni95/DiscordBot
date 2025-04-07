8090c7413205dd78766094b63d03083f26b6d18e
Nayan <nayanunni95@gmail.com>
2025-04-07 21:23:19 +0530

    add initial command and some minor config

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ from discord.ext import commands
 
 token = decouple.config("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix="/", intents=discord.Intents.all(), case_insensitive=True, self_bot=True)
+bot = commands.Bot(
+    command_prefix="/",
+    intents=discord.Intents.all(),
+    case_insensitive=True,
+    self_bot=True,
+)
+
+
+@bot.event
+async def on_ready():
+    print("Bot is ready to use!")
+    await bot.tree.sync()
+
+
+@bot.tree.command(name="ping", description="Check is the bot online")
+async def ping(interaction):
+    await interaction.response.send_message("Pong!", ephemeral=True, delete_after=5)
+
 
 bot.run(token)
